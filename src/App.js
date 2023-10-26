af8ea05c070ccd1575ac0ab1187eafceed1c49be
Samuel Bageot <samuel.bageot@gmail.com>
2023-10-26 14:07:34 +0200

    config

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./customBlocks/custom_Blocks";
+import React, { useState } from "react";
+import { BlocklyWorkspace } from "react-blockly";
+import Blockly from "blockly";
 
-function App() {
+export default function App() {
+  const [xml, setXml] = useState("");
+  const [javascriptCode, setJavascriptCode] = useState("");
+
+  const initialXml =
+    '<xml xmlns="http://www.w3.org/1999/xhtml"><block type="text" x="70" y="30"><field name="TEXT"></field></block></xml>';
+  const toolboxCategories = {
+    kind: "categoryToolbox",
+    contents: [
+      {
+        kind: "category",
+        name: "Logic",
+        colour: "#5C81A6",
+        contents: [
+          {
+            kind: "block",
+            type: "controls_if",
+          },
+          {
+            kind: "block",
+            type: "logic_compare",
+          },
+        ],
+      },
+      {
+        kind: "category",
+        name: "Math",
+        colour: "#5CA65C",
+        contents: [
+          {
+            kind: "block",
+            type: "math_round",
+          },
+          {
+            kind: "block",
+            type: "math_number",
+          },
+        ],
+      },
+      {
+        kind: "category",
+        name: "Custom",
+        colour: "#5CA699",
+        contents: [
+          {
+            kind: "block",
+            type: "new_boundary_function",
+          },
+          {
+            kind: "block",
+            type: "return",
+          },
+        ],
+      },
+    ],
+  };
+  function workspaceDidChange(workspace) {
+    const code = Blockly?.JavaScript?.workspaceToCode(workspace);
+    setJavascriptCode(code);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BlocklyWorkspace
+        toolboxConfiguration={toolboxCategories}
+        initialXml={initialXml}
+        className="fill-height"
+        workspaceConfiguration={{
+          grid: {
+            spacing: 20,
+            length: 3,
+            colour: "#ccc",
+            snap: true,
+          },
+        }}
+        onWorkspaceChange={workspaceDidChange}
+        onXmlChange={setXml}
+      />
+      <pre id="generated-xml">{xml}</pre>
+      <textarea
+        id="code"
+        style={{ height: "200px", width: "400px" }}
+        value={javascriptCode}
+        readOnly
+      ></textarea>
+    </>
   );
 }
-
-export default App;
